1c078f38abe271da62f09fa94972d93a9115f1f1
Aleksandr Savichev <asavichev@fors.ru>
2018-08-02 11:07:27 +0300

    commit test

--- Lesson02/script.sh
+++ Lesson02/script.sh
@@ -10,6 +10,13 @@ echo "Create raid..." >> ${LOG}
 mdadm --zero-superblock /dev/sd{b,c,d,e,f,g} >> ${LOG}
 mdadm --create /dev/md0 --level 5 -n 6 /dev/sd{b,c,d,e,f,g} >> ${LOG}
 
+cat /proc/mdstat  >> ${LOG}
+
+echo "Sleep 30 sec..."
+sleep 30
+
+cat /proc/mdstat  >> ${LOG}
+
 for i in {1..5}
 do
 sgdisk -g -n ${i}::+200M /dev/md0 >> ${LOG}
@@ -19,11 +26,13 @@ t=`ls /dev/md0p*| awk '{print $0;}'`
 
 for var in $t
 do
+ echo "Create FS $var ..."
+ echo "Create FS $var ..." >> ${LOG}
  n=${var:9:1}
  mkdir /u0${n} 
- mkfs.ext3 $var
+ mkfs.ext3 $var >> ${LOG}
  echo "$var /u0${n}   ext3    defaults   0 0" >> /etc/fstab
- sleep 10
+ sleep 5
  mount /u0${n}
 done
 
